1617370b3d971c09a7c341bf55580c990cad8a35
conorhastings <hastings.conorm@gmail.com>
2015-09-12 17:40:59 -0400

    goose speak

--- index.js
+++ index.js
@@ -82,6 +82,23 @@ var ShowCase = React.createClass({
 		};
 	},
 
+	componentDidMount: function() {
+		if(window.webkitSpeechRecognition) {
+			let recognizeSpeech = new webkitSpeechRecognition();
+			recognizeSpeech.continuous = true;
+			recognizeSpeech.lang = "en-US";
+			recognizeSpeech.start();
+			recognizeSpeech.onresult = (e) => {
+				let resultsLength = e.results.length;
+				let recognizedSpeech = e.results[resultsLength - 1][0].transcript;
+				let isGoose = recognizedSpeech === "goose";
+				if(isGoose) {
+					this.setState({gooseOn: true});
+				}
+			};
+		}
+	},
+	
 	toggleGoose: function() {
 		this.setState({
 			gooseOn: !this.state.gooseOn
